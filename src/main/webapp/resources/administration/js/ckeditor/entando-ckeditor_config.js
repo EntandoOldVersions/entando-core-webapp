69677752aba052575725b002de8eac4feafab347
Andrea Dessì <a.dessi@entando.com>
2013-10-01 18:24:41 +0200

    importing ckeditor 3.6.6.1 rev7696

--- src/main/webapp/resources/administration/js/ckeditor/entando-ckeditor_config.js
+++ src/main/webapp/resources/administration/js/ckeditor/entando-ckeditor_config.js
@@ -1,12 +1,10 @@
 CKEDITOR.editorConfig = function(config) {
-	//config.language = 'fr';
+	config.removePlugins = 'link'; //this plugin is disabled because we have entandolink
 	config.extraPlugins = 'entandolink';
-	//config.uiColor = '#AADC6E';
-	config.toolbar = [['Bold', 'Italic', '-', 'NumberedList','BulletedList', '-', 'entandolink', 'Unlink', '-', 'Undo','Redo', '-', 'Table', '-', 'Source']];
+	config.toolbar = [['Bold', 'Italic', '-', 'NumberedList','BulletedList', '-', 'entandolink', '-', 'Undo','Redo', '-', 'Table', '-', 'Source']];
 	config.height = 250;
 	config.forcePasteAsPlainText = true;
-	config.docType = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">';
+	config.docType = '<!DOCTYPE html>';
 	config.pasteFromWordNumberedHeadingToList = true;
 	config.pasteFromWordPromptCleanup = true;
-	config.removePlugins = 'link';
 };
