a4656374c4287c7f73b4920e831f0aab3074fbbc
Andrea Dessì <a.dessi@entando.com>
2013-10-01 16:46:10 +0200

    ckeditor: disabled link plugin

--- src/main/webapp/resources/administration/js/ckeditor/entando-ckeditor_config.js
+++ src/main/webapp/resources/administration/js/ckeditor/entando-ckeditor_config.js
@@ -8,4 +8,5 @@ CKEDITOR.editorConfig = function(config) {
 	config.docType = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">';
 	config.pasteFromWordNumberedHeadingToList = true;
 	config.pasteFromWordPromptCleanup = true;
+	config.removePlugins = 'link';
 };
